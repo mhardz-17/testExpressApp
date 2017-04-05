3b59985d614c1745f86ec895d71416cbb9808a8d
Mardy <mhardz07@gmail.com>
2017-04-05 18:10:25 +0800

    Add updates

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var db = require('../lib/db');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    db.query('SELECT * FROM employees LIMIT 10', function (err, results) {
+    db.query('SELECT * FROM employees LIMIT 10 ORDER BY firstname ASC', function (err, results) {
         //console.log(err);
         //console.log(results);
         var response = {users: [], hasError: false,msg: ''};
@@ -23,23 +23,59 @@ router.get('/', function (req, res, next) {
     })
 });
 
-router.get('/get/:id', function (req, res, next) {
-    //var users = [{firstname: 'Mardy', middlename: 'Artisano', lastname: 'dela Cruz'},
-    //    {firstname: 'Mardy', middlename: 'Artisano', lastname: 'dela Cruz'},
-    //    {firstname: 'Mardy', middlename: 'Artisano', lastname: 'dela Cruz'}];
-    var users = [];
-    db.query('SELECT * FROM employees LIMIT 10', function (err, results) {
-        //console.log(err);
-        //console.log(results);
-        if (err) {
-            console.log(err)
-            users = [];
-            res.render('users/index', {title: 'Show User', users: []})
-        } else {
-            res.render('users/index', {title: 'Show User', users: results})
-        }
-    })
+router.get('/search', function (req, res, next) {
+    var body = req.body;
 
-})
+    console.log(body)
+
+    //db.query('SELECT * FROM employees LIMIT 10 ORDER BY firstname ASC', function (err, results) {
+    //    //console.log(err);
+    //    //console.log(results);
+    //    var response = {users: [], hasError: false,msg: ''};
+    //    if (err) {
+    //        response.hasError = true;
+    //        response.hasError = 'There is problem in the server.';
+    //    } else {
+    //        response.users = results;
+    //    }
+    //
+    //    res.format({
+    //        json: function () {
+    //            res.send(response)
+    //        }
+    //    })
+    //})
+});
+
+router.get('/:id', function (req, res, next) {
+
+    var users = [];
+    var id = req.param.id;
+    if(!id) {
+        res.format({
+            json: function () {
+                res.send({hasError: true,msg: 'Id does not exists.'})
+            }
+        })
+    } else {
+        db.query('SELECT * FROM employees WHERE id = ?', [id], function (err, results) {
+            //console.log(err);
+            //console.log(results);
+            var response = {user: [], hasError: false,msg: ''};
+            if (err) {
+                response.hasError = true;
+                response.hasError = 'There is problem in the server.';
+            } else {
+                response.user = results;
+            }
+
+            res.format({
+                json: function () {
+                    res.send(response)
+                }
+            })
+        })
+    }
+});
 
 module.exports = router;
